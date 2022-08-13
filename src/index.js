59074127fc8b4f5834cffecce88a9a1d7d1dfaf3
anitazhin <anitazhin@gmail.com>
2022-08-13 15:40:07 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    nArr = Array.from(String(n));
   
+    if (nArr[0] === '-') {
+      noMin = nArr.shift(); 
+      noMinRev = nArr.reverse().join('');
+      return noMinRev;
+    } else {
+      return n.toString().split("").reverse().join("");
+    }
 }
